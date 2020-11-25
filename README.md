2a7b3eaaf588425beeb314d3b38b08a91872b496
aishwarya609 <72502756+aishwarya609@users.noreply.github.com>
2020-11-25 16:00:07 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# 21p
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,52 @@
+var wall,thickness
+var bullet,speed,weight
+
+
+
+
+function setup() {
+createCanvas(1600,400);
+
+speed=random(223,321);
+weight=random(30,52);
+thickness = random(22,83);
+
+  bullet=createSprite(50, 200, 50, 50);
+  bullet.velocityX = speed;
+  bullet.shapeColor = color(255);
+
+wall=createSprite(1200,200,thickness,height/2);
+wall.shapeColor=(230,230,230);
+
+
+}
+
+function draw() {
+  background(0);
+  
+  if(hasCollided(bullet,wall)){
+    bullet.velocityX = 0;
+    var deformation = 0.5*weight*speed*speed/22509;
+ if(deformation>180)
+ {
+    wall.shapeColor = color(255,0,0);
+  }
+  if(deformation <180 && deformation>100){
+  wall.shapeColor = color(230,230,0);
+ }
+  if(deformation<100);{
+    wall.shapeColor(0,255,0);
+  }
+  } 
+   drawSprites();
+}
+function hasCollided (lbullet,lwall) 
+ {
+bulletRightEdge = lbullet.x + lbullet.width;
+wallLeftEdge = lwall.x;
+if(bulletRightEdge >= wallLeftEdge )
+{
+  return true;
+}
+return false;
+ }
